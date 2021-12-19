81480e88390f71e5e80d470a141f3045e1c6fd42
eunseochoi <ellenchoi98@outlook.com>
2021-12-19 00:04:39 -0500

    WORKING SCRIPT LETS GO

--- database-initialization/load_data.py
+++ database-initialization/load_data.py
@@ -1,16 +1,37 @@
-import pymysql.cursors 
+import pymysql.cursors
 import pandas as pd
 
 data = pd.read_csv (r'/Users/ellenchoi/Desktop/short_datacsv/company.csv')   
 df = pd.DataFrame(data)
 
 print(df)
-db=pymysql.connect(user='j387wu', passwd='terriblepassworD123!',
+connection = pymysql.connect(user='j387wu', password='terriblepassworD123!',
                               host='marmoset04.shoshin.uwaterloo.ca',
-                              db='db356_j387wu')
+                              database='db356_j387wu',
+                              cursorclass=pymysql.cursors.DictCursor)
+with connection:
+    with connection.cursor() as cursor:
+        # Create a new record
+        sql = '''
+        CREATE TABLE Companies (
+  companyID BIGINT NOT NULL AUTO_INCREMENT,
+  symbol VARCHAR(5) UNIQUE, -- no stock tickers in the dataset have more than 5 chars
+  industryID BIGINT,
+  summary VARCHAR(255),
+  dateFounded DATETIME,
+  numberOfEmployees int,
+  fiscalDateEnd DATETIME,
+  PRIMARY KEY (companyID)
+)'''
+        cursor.execute(sql)
 
+    # connection is not autocommit by default. So you must commit to save
+    # your changes.
+    connection.commit()
 
-cursor = db.cursor()
-cursor.execute("SELECT VERSION()")
-db.close()
-
+    with connection.cursor() as cursor:
+        # Read a single record
+        sql = "SELECT `companyID` FROM `Companies`"
+        cursor.execute(sql)
+        result = cursor.fetchone()
+        print(result)
