048dbcaecc8e83975ffed41e19ca2b97fe46975a
Ellen Choi <ellen.choi@coinbase.com>
2021-12-20 15:59:57 -0500

    working script for creating tables

--- database-initialization/create_tables.sql
+++ database-initialization/create_tables.sql
@@ -1,107 +1,109 @@
 CREATE TABLE Companies (
-  companyID BIGINT NOT NULL AUTO_INCREMENT,
+  companyID  int NOT NULL AUTO_INCREMENT,
   symbol VARCHAR(5) UNIQUE, -- no stock tickers in the dataset have more than 5 chars
-  industryID BIGINT,
+  industryID INT,
   summary VARCHAR(255),
   dateFounded DATETIME,
   numberOfEmployees int,
   fiscalDateEnd DATETIME,
-  location
-
   PRIMARY KEY (companyID)
 );
 
 CREATE TABLE Sectors (
-  sectorID BIGINT NOT NULL AUTO_INCREMENT,
+  sectorID  int NOT NULL AUTO_INCREMENT,
   sectorName VARCHAR(255) NOT NULL UNIQUE,
   description VARCHAR(255),
 
-  PRIMARY KEY (secotrName)
+  PRIMARY KEY (sectorID)
 );
 
 CREATE TABLE Industries (
-  industryID BIGINT NOT NULL AUTO_INCREMENT,
+  industryID  int NOT NULL AUTO_INCREMENT,
   name VARCHAR(255) NOT NULL UNIQUE,
   description VARCHAR(255),
-
+  sectorID INT,
   PRIMARY KEY (industryID),
-  FOREIGN KEY (sector) REFERENCES Sectors(sectorID)
+  FOREIGN KEY (sectorID) REFERENCES Sectors(sectorID)
 );
 
 CREATE TABLE FiscalYear (
-  fiscalYear int(4) -- year is always 4 digits
-
-  FOREIGN KEY (company) REFERENCES Companies(companyID),
-  FOREIGN KEY (fiscalYear)
+  fyID  int NOT NULL AUTO_INCREMENT,
+  fiscalYear int(4), -- year is always 4 digits
+  PRIMARY KEY (fyID)
 );
 
 CREATE TABLE DayStats (
-  date DATETIME,
-  dayOfWeek VARCHAR((8),
+  dayStatID int NOT NULL AUTO_INCREMENT,
+  date DATETIME NOT NULL,
+  dayOfWeeks int(1),
   volume int,
-  open decimal,
-  high decimal,
-  low decimal,
-  close decimal,
-  adjclose  DECIMAL,
-
-  FOREIGN KEY (company) REFERENCES Companies(companyID),
-  FOREIGN KEY (fiscalYear) REFERENCES FiscalYear(fiscalYear)
-);
-
+  open float,
+  high float,
+  low float,
+  close float,
+  adjclose  float,
+  companyID INT,
+  fiscalYear int(4),
+  PRIMARY KEY (dayStatID),
+  FOREIGN KEY (companyID) REFERENCES Companies(companyID)
+  );
+  
 CREATE TABLE Leaders (
-  leaderID BIGINT NOT NULL AUTO_INCREMENT,
-  name VARCHAR,
-  role VARCHAR,
+  leaderID  int NOT NULL AUTO_INCREMENT,
+  name VARCHAR(100),
+  role VARCHAR(100),
   age INT,
-  gender VARCHAR,
+  gender VARCHAR(10),
   startDate DATETIME,
   endDate DATETIME,
   founder BOOLEAN,
-
+  company VARCHAR(255),
   PRIMARY KEY (leaderID)
 );
 
 CREATE TABLE IPOs (
   ipoDate DATETIME,
-  lastSale DECIMAL,
+  lastSale float,
   CEOInChargeDuringIPO BOOLEAN,
   presidentInChargeDuringIPO BOOLEAN,
-  revenue DECIMAL,
-  netIncome DECIMAL,
-  lastFiscalYearGrowth DECIMAL,
+  revenue float,
+  netIncome float,
+  lastFiscalYearGrowth float,
   daysBetterThanSP INT,
   daysProfit INT,
   daysProfitGrouped INT,
   exactDifference INT,
-  netIncomeYearBeforeIPO DECIMAL,
-
-  FOREIGN KEY (fiscalYear) REFERENCES FiscalYear(fiscalYear),
+  netIncomeYearBeforeIPO float,
+  fiscalYear int(4),
+  sectorID INT,
+  industryID INT,
+  companyID INT,
+  -- FOREIGN KEY (fiscalYear) REFERENCES FiscalYear(fiscalYear),
   FOREIGN KEY (sectorID) REFERENCES Sectors(sectorID),
   FOREIGN KEY (industryID) REFERENCES Industries(industryID),
   FOREIGN KEY (companyID) REFERENCES Companies(companyID)
 );
 
 CREATE TABLE AnnualReports (
-  revenue DECIMAL,
-  revenueGrowth DECIMAL,
-  costOfRevenue DECIMAL,
-  grossProfit DECIMAL,
-  operatingExpenses DECIMAL,
-  operatingIncome DECIMAL,
-  earningsBeforeTax DECIMAL,
-  incomeTaxExpense DECIMAL,
-  netIncome DECIMAL,
-  dividendPerShare DECIMAL,
-  grossMargin DECIMAL,
-  profitMargin DECIMAL,
-  freeCashFlowMargin DECIMAL,
-  totalCurrentAssets DECIMAL,
-  taxAssets DECIMAL,
-  payables DECIMAL,
-  totalDebt DECIMAL,
-
-  FOREIGN KEY (companyID) REFERENCES Companies(companyID),
-  FOREIGN KEY (fiscalYear) REFERENCES FiscalYear(year) 
-);
+  revenue float,
+  revenueGrowth float,
+  costOfRevenue float,
+  grossProfit float,
+  operatingExpenses float,
+  operatingIncome float,
+  earningsBeforeTax float,
+  incomeTaxExpense float,
+  netIncome float,
+  dividendPerShare float,
+  grossMargin float,
+  profitMargin float,
+  freeCashFlowMargin float,
+  totalCurrentAssets float,
+  taxAssets float,
+  payables float,
+  totalDebt float,
+  companyID INT,
+  fiscalYear int(4),
+  FOREIGN KEY (companyID) REFERENCES Companies(companyID)
+  );
 
