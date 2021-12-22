40360e253b72bf594b4140ef97b8a3b383a78149
wu-jeffrey <jeff.jiang.wu@gmail.com>
2021-12-22 13:44:58 -0500

    fix: companies query to leverage FKs

--- web-server/api/companies.js
+++ web-server/api/companies.js
@@ -15,9 +15,9 @@ router.get('/', (req, res, next) => {
 
 router.get('/:companyID', (req, res, next) => {
   db.query(`
-    SELECT symbol, companyName, companyID, sectorName, industryName, numberOfEmployees, city, stateCountry
-    FROM Companies
-    WHERE companyID = ${req.params.companyID}
+    SELECT symbol, companyName, companyID, numberOfEmployees, city, stateCountry, I.name AS industryName, S.sectorName
+      FROM (Companies AS C INNER JOIN Industries AS I ON C.industryID = I.industryID) INNER JOIN Sectors AS S ON I.sectorID = S.sectorID
+      WHERE companyID = ${req.params.companyID}
     `,
     function (err, results) {
       if (err) return next(err);
